84bbc5c928ff2aee992c1095f6093263c0b5e21c
R-Betson <116415297+R-Betson@users.noreply.github.com>
2024-08-30 15:58:32 -0500

    Adding javadoc comments
    Javadoc comments added to class

--- cen4802/src/cen4802/FibSeqAssignment1.java
+++ cen4802/src/cen4802/FibSeqAssignment1.java
@@ -4,6 +4,11 @@ package cen4802;
 
 public class FibSeqAssignment1 {
 
+	/*
+	 * Recursive method to find the nth term in the Fibonacci Sequence
+	 * @param n is the position in the Fibonacci Sequence 
+	 * @return The nth term of the Fibonacci Sequence, should return the sum of (n-1) and (n-2).
+	 */
 	public static int fibonacci(int n) {
 
 		if (n <= 1) {
@@ -15,6 +20,10 @@ public class FibSeqAssignment1 {
 		return fibonacci(n - 1) + fibonacci(n - 2);
 
 	}
+	
+	/*
+	 * The main method executes the Fibonacci Sequence method for a given number, in this case 10, and prints the result
+	 */
 
 	public static void main(String[] args) {
 
