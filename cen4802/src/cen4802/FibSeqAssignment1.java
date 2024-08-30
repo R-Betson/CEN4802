7557d8beeaa35a96f3744ff0f204edd0992af64d
R-Betson <116415297+R-Betson@users.noreply.github.com>
2024-08-30 15:43:30 -0500

    Adding class to the main branch in GitHub repo

---
+++ cen4802/src/cen4802/FibSeqAssignment1.java
@@ -0,0 +1,29 @@
+//Rebecca Betson
+
+package cen4802;
+
+public class FibSeqAssignment1 {
+
+	public static int fibonacci(int n) {
+
+		if (n <= 1) {
+
+			return n;
+
+		}
+
+		return fibonacci(n - 1) + fibonacci(n - 2);
+
+	}
+
+	public static void main(String[] args) {
+
+		int n = 10;
+
+		int result = fibonacci(n);
+
+		System.out.println("The " + n + "th term of the Fibonacci Sequence is " + result + ".");
+
+	}
+
+}
